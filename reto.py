fde6a551852f1f55bd00789ea5608505032db0ba
Hedley Knewjen Quintana <hedleypanama@gmail.com>
2022-05-08 08:07:42 -0500

    Add files via upload

---
+++ reto.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+'''
+Debe completar las dos líneas de código faltantes de este script. No se 
+permite crear:
+1- Crear más líneas de código.
+2- Ningún tipo de función (NINGUNA).
+3- Usar diccionarios ni el punto y coma.
+4- Recomiendo no usar ningún tipo de información aparte de la que está en este script
+
+Estos son los signos del año cero y de los doce primeros años de la era cristiana:
+Año 0: "Mono"
+Año 1: "Gallo"
+Año 2: "Perro"
+Año 3: "Cerdo"
+Año 4: "Rata"
+Año 5: "Buey"
+Año 6: "Tigre"
+Año 7: "Conejo"
+Año 8: "Dragón"
+Año 9: "Serpiente"
+Año 10: "Caballo"
+Año 11: "Cabra"
+Año 12: "Mono"
+
+Resultados esperados: 
+1978: "Caballo"
+1990: "Caballo"
+2000: "Dragón"
+2020: "Rata"
+2022: "Perro"
+5301: "Serpiente"
+
+'''
+try:
+    while True:
+       año=abs(int(input('Debe ingresar el año y se le dirá a qué signo del horóscopo chino pertenece: ')))
+       # Complete la primera línea de código
+       # Cree la variable resultado para completar la segunda línea de código
+       print(resultado)
+except:
+    print("Usted ha ingresado un valor inválido")
